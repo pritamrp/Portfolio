f9430c3facf83cb281dd390bd1b5dd1f352f8e20
Pritam Pandit <91716504+pritamrp@users.noreply.github.com>
2024-10-02 16:15:40 -0400

    Update skills-chart-component.js

--- components/skills-chart-component.js
+++ components/skills-chart-component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import styles from '../styles/skills-chart-css-module.css';
+import styles from '../styles/skills-chart.module.css';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const SkillsChart = () => {
