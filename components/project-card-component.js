a9a796fc84856a7b3fb1c0d844d669b094e06aa1
Pritam Pandit <91716504+pritamrp@users.noreply.github.com>
2024-10-02 15:15:14 -0400

    Update project-card-component.js

--- components/project-card-component.js
+++ components/project-card-component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/ProjectCard.module.css';
+import styles from '../styles/project-card-css-module.css';
 
 const ProjectCard = ({ title, description, technologies, imageUrl }) => {
   return (
