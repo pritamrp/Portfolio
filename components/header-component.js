c9a90657cfa2faf2a9510a4619a5ce9b6f76949c
Pritam Pandit <91716504+pritamrp@users.noreply.github.com>
2024-10-02 16:14:07 -0400

    Update header-component.js

--- components/header-component.js
+++ components/header-component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../styles/header-css-module.css';
+import styles from '../styles/header.module.css';
 
 const Header = ({ darkMode, toggleDarkMode }) => {
   return (
