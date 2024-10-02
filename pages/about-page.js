00d16e1dcbdd4e5ca7a8d4b42b5c9ce41b4377f8
Pritam Pandit <91716504+pritamrp@users.noreply.github.com>
2024-10-02 09:35:14 -0400

    Update about-page.js

--- pages/about-page.js
+++ pages/about-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '../components/layout-component.js';
 import styles from '../styles/About.module.css';
 
 export default function About() {
