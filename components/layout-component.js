5738024372f9a6096c05468130c600a25ba0a0a7
Pritam Pandit <91716504+pritamrp@users.noreply.github.com>
2024-10-02 09:28:50 -0400

    Update layout-component.js

--- components/layout-component.js
+++ components/layout-component.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.css';
+import styles from '../styles/layout-css-module.css';
 
 const Layout = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
